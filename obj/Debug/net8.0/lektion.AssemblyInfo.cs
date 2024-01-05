1f975f91797b5ea119251c11408d79134153706b
Jesper Wallén <jesperwallen2001@gmail.com>
2024-01-05 13:47:48 +0100

    first commit

--- obj/Debug/net8.0/lektion.AssemblyInfo.cs
+++ obj/Debug/net8.0/lektion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lektion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41490794d81e674a29fbcd0ac28e3c7e8e223ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("lektion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lektion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
